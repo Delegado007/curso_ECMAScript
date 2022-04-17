5f6061ef3ff41023a1e0803c662c407bf3792661
Delegado <maxharper2013@gmail.com>
2022-04-17 02:02:52 -0300

    agregando async await de es8

--- ES8/index.js
+++ ES8/index.js
@@ -30,3 +30,29 @@ const string = "hello";
 console.log(string.padStart(7, "hi")); //hihello -> output
 // console.log(string.padStart(cantiCaracteresMax, "cadena para completar"));
 console.log(string.padEnd(12, " --------")); // hello ------ -> output
+
+//async y await
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true //cambiar a false para ejecutar el reject
+      ? setTimeout(() => resolve("Hello World"), 3000)
+      : reject(new Error("Test Error"));
+  });
+};
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+};
+anotherFunction();
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+helloAsync();
