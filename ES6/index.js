a8fd727ed1d234a1cd5b0e497fa5463afb0036a8
Delegado <maxharper2013@gmail.com>
2022-04-16 23:08:21 -0300

    casi terminado con es6

--- ES6/index.js
+++ ES6/index.js
@@ -21,3 +21,92 @@ let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//multilinea
+let lorem =
+  "Lorem Ipsum is simply dummy text of the printing \n" +
+  "and typesetting industry.";
+console.log(lorem);
+//es6
+let lorem2 = `otra frase epica que necesitamos
+y seguimos abajo`;
+console.log(lorem2);
+//destructuracion de objetos
+let person = {
+  name: "oscar",
+  age: 32,
+  country: "mx",
+};
+console.log(person.name, person.age);
+
+let { country } = person;
+console.log(country);
+//operador de propagacion
+let team1 = ["oscar", "Julian", "Ricardo"];
+let tema2 = ["Valeria", "Yesica", "camila"];
+
+let education = ["David", ...team1, ...tema2];
+console.log(education);
+
+//let y su scope
+{
+  var globalVar = "Global var"; //disponible de forma global
+}
+{
+  let globalLet = "Global Let";
+  console.log(globalLet);
+}
+console.log(globalVar); //let inaccesible desde afuera
+
+const a = "b";
+a = "b"; // no prmite la reasignacion a una constante
+console.log(a);
+
+// propiedad de objetos mejorada
+let name = "Franco";
+let age = 32;
+//es5
+obj = { name: name, age: age };
+obj2 = { name, age };
+//es6
+console.log(obj);
+console.log(obj2);
+
+//arrow function
+const names = [
+  { name: "Franco", age: 27 },
+  { name: "Yesica", age: 27 },
+];
+//es5
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+});
+//es6
+let listOfNames2 = names.map((item) => console.log(item.name));
+const listOfNames3 = (name, age, country) => {
+  console.log("bloque de codigo a ejecutar");
+};
+
+const listOfNames4 = (name) => {
+  console.log("bloque de codigo");
+};
+
+const square = (num) => num * num;
+
+// promesas para trabajar asincronismo
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      //cambia a false para mandar a reject
+      resolve("Hey!");
+    } else {
+      reject("Ups!!");
+    }
+  });
+};
+
+helloPromise()
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
+
+//clases
